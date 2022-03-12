c716a8a1a90a090426bf0888ef58eee543bc1b10
James Wallace <4538317+NZLWally@users.noreply.github.com>
2022-03-12 18:08:12 +1300

    Building out OO Twenty One classes and instance methods

--- lesson_5/oo_twenty_one/1.0_oo_twenty_one.rb
+++ lesson_5/oo_twenty_one/1.0_oo_twenty_one.rb
@@ -4,35 +4,92 @@ end
 
 class Participant
   include Hand
-
-  def initialize; end
-
-  def hit; end
-
-  def stay; end
-
-  def busted?; end
-end
-
-class Player
   attr_reader :name
 
   def initialize(name)
-    super()
+    @hand = []
     @name = name
+  end
+
+  def display_hand
+    @hand.each do |card|
+      puts card.to_s
+    end
+  end
+
+  def display_hand_value
+    puts "#{@name} has a hand value of [number]."
+  end
+
+  def busted?
+    false
   end
 end
 
-class Dealer
-  def deal_card; end
+class Player < Participant; end
+
+class Dealer < Participant
+  def initialize
+    super('Dealer')
+  end
+
+  def deal_card(participant); end
+
+  def display_first_card
+    first_card = @hand.first
+    puts "#{@name} has #{first_card}"
+  end
 end
 
 class Deck
-  def initialize; end
+  SUITS = ['Diamonds', 'Hearts', 'Spades', 'Clubs']
+  FACE_VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+  CARD_VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11] # [1, 10]]
+
+  attr_accessor :deck
+
+  def initialize
+    @deck = create_new_deck
+  end
+
+  def to_s
+    deck.each do |card|
+      puts card
+    end
+  end
+
+  def shuffle_deck!
+    @deck.shuffle!
+  end
+
+  private
+
+  def create_new_deck
+    temp_deck = []
+
+    SUITS.each do |suit|
+      FACE_VALUES.each_with_index do |face_value, index|
+        card_value = CARD_VALUES[index]
+        new_card = Card.new(suit, face_value, card_value)
+        temp_deck.push(new_card)
+      end
+    end
+    temp_deck
+  end
 end
 
 class Card
-  def initialize; end
+  attr_reader :suit, :face_value, :card_value
+
+  def initialize(suit, face_value, card_value)
+    @suit = suit
+    @face_value = face_value
+    @card_value = card_value
+  end
+
+  def to_s
+    "#{face_value} of #{suit}"
+  end
 end
 
 class Game
@@ -82,31 +139,49 @@ class Game
   end
 
   def game_loop
-    deal_cards
-    display_initial_cards
+    create_shuffled_deck
+    deal_initial_cards
+    display_hands_for_player
     player_turn
     dealer_turn
     display_game_result
   end
 
-  def deal_cards; end
+  def create_shuffled_deck
+    @deck = Deck.new
+    @deck.shuffle_deck!
+  end
 
-  def display_initial_cards; end
-
-  def display_player_cards; end
-
-  def display_dealer_card; end
-
-  def player_turn
-    loop do
-      display_participant_cards
-      move = choose_move
-      break if move == 'stay' || move == 's'
-      deal_card
+  def deal_initial_cards
+    2.times do
+      @dealer.deal_card(@player)
+      @dealer.deal_card(@dealer)
     end
   end
 
-  def display_participant_cards; end
+  def display_hands_for_player
+    @dealer.display_first_card
+    @dealer.display_hand_value
+    @player.display_hand
+    @player.display_hand_value
+  end
+
+  def player_turn
+    loop do
+      move = choose_move
+      break if move == 'stay' || move == 's'
+      @dealer.deal_card(@player)
+      break if @player.busted?
+      display_hands_for_player
+    end
+  end
+
+  def display_participant_cards
+    @dealer.display_hand
+    @dealer.display_hand_value
+    @player.display_hand
+    @player.display_hand_value
+  end
 
   def choose_move
     move = nil
@@ -121,11 +196,23 @@ class Game
     move
   end
 
-  def deal_card; end
+  def dealer_turn
+    display_participant_cards
+    loop do
+      move = 'stay' # dealer_move_logic
+      puts "#{@dealer.name} #{move}."
+      display_participant_cards
+      break if move == 'stay'
+      @dealer.deal_card(@dealer)
+      break if @dealer.busted?
+    end
+  end
 
-  def dealer_turn; end
-
-  def display_game_result; end
+  def display_game_result
+    # determine winner or tie
+    # display winner or tie
+    puts 'You win!'
+  end
 
   def play_again?
     answer = nil
