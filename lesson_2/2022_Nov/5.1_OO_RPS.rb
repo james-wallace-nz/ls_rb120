e9fca73471edb021d84b674df4a974ef4be2c19e
James Wallace <4538317+NZLWally@users.noreply.github.com>
2022-12-04 14:00:23 +1300

    Lesson 2 OO RPS

--- lesson_2/2022_Nov/5.1_OO_RPS.rb
+++ lesson_2/2022_Nov/5.1_OO_RPS.rb
@@ -1,46 +1,43 @@
 class Player
   attr_accessor :move, :name
 
-  def initialize(player_type = :human)
-    @player_type = player_type
-    @move = nil
+  def initialize          # called in method lookup hierachy because no `initialize` in sub-classes
+    # @move = nil         # initialized to nil by default (through attr_accessor?)
     set_name
   end
+end
 
+class Human < Player
   def set_name
-    if human?
-      n = nil
-      loop do
-        puts "What's your name?"
-        n = gets.chomp
-        break unless n.empty?
-        puts "Sorry, must enter a value."
-      end
-      self.name = n
-    else
-      self.name = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number 5'].sample
+    n = nil
+    loop do
+      puts "What's your name?"
+      n = gets.chomp
+      break unless n.empty?
+      puts "Sorry, must enter a value."
     end
+    self.name = n
   end
 
   def choose
-    if human?
-      choice = nil
-      loop do
-        puts "Please choose rock, paper, or scissors:"
-        choice = gets.chomp
-        break if ['rock', 'paper', 'scissors'].include? choice
-        puts "Sorry, invalid choice."
-      end
-      self.move = choice
-    else
-      self.move = ['rock', 'paper', 'scissors'].sample
+    choice = nil
+    loop do
+      puts "Please choose rock, paper, or scissors:"
+      choice = gets.chomp
+      break if ['rock', 'paper', 'scissors'].include? choice
+      puts "Sorry, invalid choice."
     end
+    self.move = choice
+  end
+end
+
+class Computer < Player
+  def set_name
+    self.name = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number 5'].sample
   end
 
-  private
-
-  def human?
-    @player_type == :human
+  def choose
+    self.move = ['rock', 'paper', 'scissors'].sample
   end
 end
 
@@ -48,8 +45,8 @@ class RPSGame
   attr_accessor :human, :computer
 
   def initialize
-    @human = Player.new
-    @computer = Player.new(:computer)
+    @human = Human.new
+    @computer = Computer.new
   end
 
   def play
@@ -106,3 +103,12 @@ class RPSGame
 end
 
 RPSGame.new.play
+
+# Is this design, with Human and Computer sub-classes, better? Why, or why not?
+  # This design is better because we don't have to specify the Player type in Player class as we can directly work with a Human or Computer class that has their own behaviour.
+
+# What is the primary improvement of this new design?
+  # The benefit is that we can treat the Human and Computer the same with the same method calls but the behaviours they exhibit are different - duck typing, polymorphism.
+
+# What is the primary drawback of this new design?
+  # We must duplicate our code in two classes and ensure both have the same behaviours
