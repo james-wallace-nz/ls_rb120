22975a1516275bd6363d98292d755e9531a21c75
James Wallace <4538317+NZLWally@users.noreply.github.com>
2024-07-03 22:20:54 +1200

    Add exercise files

---
+++ Object Oriented Programming Exercises/2023_Aug/1_oo_basics_1/1.rb
@@ -0,0 +1,16 @@
+# Update the following code so that, instead of printing the values, each statement prints the name of the class to which it belongs.
+
+
+puts "Hello".class
+puts 5.class
+puts [1, 2, 3].class
+
+# Expected output:
+
+# String
+# Integer
+# Array
+
+# All values in the example are objects. Each object is an instance of a class: "Hello is a String, 5 is an Integer, and [1, 2, 3] is an Array.
+
+# To find the class an object belongs to, we invoke the #class method.
---
+++ Object Oriented Programming Exercises/2023_Aug/1_oo_basics_1/3.rb
@@ -0,0 +1,13 @@
+# Using the code from the previous exercise, create an instance of Cat and assign it to a variable named kitty.
+
+# Code:
+
+class Cat
+
+end
+
+kitty = Cat.new
+
+# To instantiate a new Cat object, we use the class method ::new. This method returns a new instance of the class it was invoked upon. We now have a Cat object.
+
+# To make use of this new Cat object, we need to assign it to a variable. In the solution, we assign the object to a variable named kitty.
